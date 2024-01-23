ff9295fd2dbada4cbbbd44b67ea59729f9786304
Lynne Liu <lynne.liukail@gmail.com>
2024-01-22 21:21:16 -0500

    finished the worked version of server, 1st version. Worked correctly with the testing code 'echo -n ftp | nc -u localhost 55000' locally. Again, need to build(make) first before running. Other thing didn't change.

--- Lab1/server.c
+++ Lab1/server.c
@@ -1,4 +1,4 @@
-// Include packages
+/* Include packages */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,43 +12,47 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    //get the server's address
-    char input[] = argv[1];
-    if(argv[1])
-    int inet_pton(argv[1]);
+    /* get the server's address */
+    /* char *input = argv[1];
+    if(argv[1]){
+        int print_to_net = inet_pton(argv[1]);
+    }else{
+        int hum_to_net = getaddrinfo(argv[1]);
+    } */
 
-    // Create a UDP socket
+    /* Create a UDP socket */
     int socket_FD = socket(AF_INET, SOCK_DGRAM, 0);
-
+    // corner case
     if (socket_FD < 0) {
         perror("socket creation failed");
         exit(EXIT_FAILURE);
     }
 
-    //get the server address information 
-    struct sockaddr_in server_addr = {0};
-    server_addr.sin_family = AF_INET; // IPv4
-    server_addr.sin_addr = htonl(INADDR_ANY);
-    server_addr.sin_port = htons(inet_pton(argv[1]));
-    
+    /* get the server address information */
+    struct sockaddr_in server_addr = {0}; // sth handle internet address; a struct
+    server_addr.sin_family = AF_INET; // set address family; specify using IPv4
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY); // specify the IP addr for socket; htonl is to ensure correct byte order for network (host to network long)
+    server_addr.sin_port = htons(atoi(argv[1])); // host to network short
 
-    // Binding the socket to the server address
+    /* Binding the socket to the server address*/ 
+    // associates socket with specific IP and port#
     int bind_result = bind(socket_FD, (const struct sockaddr *)&server_addr, sizeof(server_addr));
 
-    //If bind fails
+    //If bind fails, try different port by user
     if(bind_result < 0){
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
 
-    // Receive messages from clients
+/* -------------------------------------------------------------------------- */
+    /* Receive messages from clients */
     char buffer[1024];
-    struct sockaddr_in cliaddr;
-    int len = sizeof(cliaddr);
+    struct sockaddr_storage client_addr;
+    socklen_t len = sizeof(client_addr);
 
     while (1) {
-        int n = recvfrom(socket_FD, (char *)buffer, 1024, MSG_WAITALL, (struct sockaddr *) &cliaddr, &len);
-        buffer[n] = '\0'; // Null-terminate the string.
+        int n = recvfrom(socket_FD, (char *)buffer, 1024, 0, (struct sockaddr *) &client_addr, &len); // receive msg from a socket
+        buffer[n] = '\0'; // Null-terminate the string
 
         // Check the message and respond
         const char *msg;
@@ -57,7 +61,7 @@ int main(int argc, char *argv[]) {
         } else {
             msg = "no";
         }
-        sendto(socket_FD, msg, strlen(msg), MSG_CONFIRM, (const struct sockaddr *) &cliaddr, len);
+        sendto(socket_FD, msg, strlen(msg), 0, (const struct sockaddr *) &client_addr, len);
     }
 
     // Close the socket
@@ -65,3 +69,4 @@ int main(int argc, char *argv[]) {
 
     return 0;
 }
+
